3da0935032b1725b5d0b08fc1c5215fbcd0e6ee5
jyroy <jyroooy@163.com>
2024-06-10 17:38:36 +0800

    bugfix

--- BERT/train.py
+++ BERT/train.py
@@ -12,12 +12,12 @@ from model.model import *
 
 BASE_DIR = Path(__file__).resolve().parent
 
-NUM_LAYERS = 12
-EMBEDDING_SIZE = 512
-HIDDEN_SIZE = 768
-EPOCHS = 10
-BATCH_SIZE = 1
-NUM_HEADS = 8
+NUM_LAYERS = 1
+EMBEDDING_SIZE = 64
+HIDDEN_SIZE = 36
+EPOCHS = 4
+BATCH_SIZE = 12
+NUM_HEADS = 4
 LOG_DIR = "./log"
 CHECKPOINT = "./ckpt/bert_final.pt"
 
@@ -25,6 +25,57 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 if torch.cuda.is_available():
     torch.cuda.empty_cache()
+
+
+def percentage(batch_size: int, max_index: int, current_index: int):
+    """Calculate epoch progress percentage
+
+    Args:
+        batch_size: batch size
+        max_index: max index in epoch
+        current_index: current index
+
+    Returns:
+        Passed percentage of dataset
+    """
+    batched_max = max_index
+    return round(current_index / batched_max * 100, 2)
+
+
+def nsp_accuracy(result: torch.Tensor, target: torch.Tensor):
+    """Calculate NSP accuracy between two tensors
+
+    Args:
+        result: result calculated by model
+        target: real target
+
+    Returns:
+        NSP accuracy
+    """
+    s = (
+        result.argmax(1) == target.argmax(1)
+    ).sum()  # ([1, 1, 0, 0, ..., 1] == [1, 0, 0, 0, ..., 1]).sum()
+    return round(float(s / result.size(0)), 2)
+
+
+def token_accuracy(
+    result: torch.Tensor, target: torch.Tensor, inverse_token_mask: torch.Tensor
+):
+    """Calculate MLM accuracy between ONLY masked words
+
+    Args:
+        result: result calculated by model
+        target: real target
+        inverse_token_mask: well-known inverse token mask
+
+    Returns:
+        MLM accuracy
+    """
+    r = result.argmax(-1).masked_select(~inverse_token_mask)
+    t = target.masked_select(~inverse_token_mask)
+    s = (r == t).sum()
+    return round(float(s / (result.size(0) * result.size(1))), 2)
+
 
 if __name__ == "__main__":
     print("Prepare dataset")
@@ -37,9 +88,9 @@ if __name__ == "__main__":
     ).to(device)
     dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-    token_criterion = nn.CrossEntropyLoss(ignore_index=0).to(device)
-    nsp_cirterion = nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.015)
+    token_criterion = nn.NLLLoss(ignore_index=0).to(device)
+    nsp_cirterion = nn.BCEWithLogitsLoss().to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0007, weight_decay=0.015)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
     batched_length = len(dataset) // BATCH_SIZE
 
@@ -55,77 +106,64 @@ if __name__ == "__main__":
         print("No checkpoint")
         start_epoch = 0
 
-    # print("Begin Training")
-    # model.train()
-    # for epoch in range(start_epoch, EPOCHS):
-    #     start_time = time.time()
-    #     print("Epoch %d 的学习率：%f" % (epoch, optimizer.param_groups[0]["lr"]))
-    #     for index, value in enumerate(dataloader):
-    #         (
-    #             masked_sentence,
-    #             inp,
-    #             mask,
-    #             inverse_token_mask,
-    #             original_target,
-    #             token_target,
-    #             nsp_target,
-    #         ) = value
-    #         optimizer.zero_grad()
+    print("Begin Training")
+    model.train()
+    for epoch in range(start_epoch, EPOCHS):
+        start_time = time.time()
+        print("Epoch %d 的学习率：%f" % (epoch, optimizer.param_groups[0]["lr"]))
+        for index, value in enumerate(dataloader):
+            (
+                masked_sentence,
+                inp,
+                mask,
+                inverse_token_mask,
+                original_target,
+                token_target,
+                nsp_target,
+            ) = value
+            optimizer.zero_grad()
 
-    #         token, nsp = model(inp, mask)
-    #         tm = inverse_token_mask.unsqueeze(-1).expand_as(token)
-    #         token = token.masked_fill(tm, 0)
+            token, nsp = model(inp, mask)
+            tm = inverse_token_mask.unsqueeze(-1).expand_as(token)
+            token = token.masked_fill(tm, 0)
 
-    #         loss_token = token_criterion(token.transpose(1, 2), token_target)
-    #         loss_nsp = nsp_cirterion(nsp, nsp_target)
+            loss_token = token_criterion(token.transpose(1, 2), token_target)
+            loss_nsp = nsp_cirterion(nsp, nsp_target)
 
-    #         loss = loss_token + loss_nsp
+            loss = loss_token + loss_nsp
 
-    #         loss.backward()
-    #         optimizer.step()
+            loss.backward()
+            optimizer.step()
 
-    #         end_time = time.time() - start_time
-    #         if index % 10 == 0:
-    #             elapsed = time.gmtime(end_time)
-    #             print(
-    #                 f"Epoch {epoch} | {index} / {batched_length} | NSP loss {loss_nsp} | Token loss {loss_token} | elapsed {time.strftime('%H:%M:%S', elapsed)}"
-    #             )
-    #         if index % 2000 == 0:
-    #             scheduler.step()
-    #         writer.add_scalar("NSP Loss", loss_nsp, index * (epoch + 1))
-    #         writer.add_scalar("TOKEN Loss", loss_token, index * (epoch + 1))
-        
-    #     torch.save(
-    #         {
-    #             "epoch": epoch,
-    #             "model_state_dict": model.state_dict(),
-    #             "optimizer_state_dict": optimizer.state_dict(),
-    #             "loss": loss,
-    #         },
-    #         CHECKPOINT,
-    #     )
-    #     print(f"Model save to {CHECKPOINT}!")
+            end_time = time.time() - start_time
+            if index % 10 == 0:
+                elapsed = time.gmtime(end_time)
+                global_step = epoch * len(dataloader) + index
+                nsp_acc = nsp_accuracy(nsp, nsp_target)
+                token_acc = token_accuracy(token, token_target, inverse_token_mask)
 
-    print("Begin Evaluation")
-    model.eval()
-    for index, value in enumerate(dataloader):
-        (
-            masked_sentence,
-            inp,
-            mask,
-            inverse_token_mask,
-            original_target,
-            token_target,
-            nsp_target,
-        ) = value
-        token, nsp = model(inp, mask)
-        print("Mask sentence: ", masked_sentence)
-        print("Original sentence: ", original_target)
-        token_softmax = torch.nn.functional.softmax(token, dim=-1)
-        top_k_pred = torch.argmax(token_softmax, dim=-1)
-        for k in range(1):
-            pred_k = top_k_pred[:, :]
-            print(pred_k)
-            for each_token in pred_k[0]:
-                word = dataset.vocab.lookup_token(each_token)
-                print(word)
+                writer.add_scalar(
+                    "NSP train accuracy", nsp_acc, global_step=global_step
+                )
+                writer.add_scalar(
+                    "Token train accuracy", token_acc, global_step=global_step
+                )
+                acc_res = f" | NSP accuracy {nsp_acc} | Token accuracy {token_acc}"
+
+                print(
+                    f"Epoch {epoch} | {index} / {len(dataloader)} {percentage(BATCH_SIZE, len(dataloader), index)}% | NSP loss {loss_nsp} | Token loss {loss_token} | elapsed {time.strftime('%H:%M:%S', elapsed)} | "
+                    + acc_res
+                )
+            writer.add_scalar("NSP Loss", loss_nsp, index * (epoch + 1))
+            writer.add_scalar("TOKEN Loss", loss_token, index * (epoch + 1))
+        scheduler.step()
+        torch.save(
+            {
+                "epoch": epoch,
+                "model_state_dict": model.state_dict(),
+                "optimizer_state_dict": optimizer.state_dict(),
+                "loss": loss,
+            },
+            CHECKPOINT,
+        )
+        print(f"Model save to {CHECKPOINT}!")
